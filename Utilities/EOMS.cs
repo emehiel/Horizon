3995821e0e140b21bf34b1f81996429a18954b8f
Eric Mehiel <emehiel@calpoly.edu>
2016-02-16 11:41:59 -0800

    EOMS

---
+++ Utilities/EOMS.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Utilities
+{
+/// <summary>
+/// An equation of motion object with the parameters governing some
+/// sort of motion of an object.
+/// @author Cory O'Connor
+/// </summary>
+    abstract class EOMS
+    {
+        /// <summary>
+        /// Computes one timestep of the equations of motion object from a specified
+		/// state after a specified period of time
+        /// </summary>
+        /// <param name="t">the time at which to evaluate the function</param>
+        /// <param name="y">the current state of the object</param>
+        /// <returns>dy the matrix result</returns>
+        public abstract Matrix this[double t, Matrix y]
+        {
+            get;
+        }
+    }
+}

1c24f8930513ab1dd452b3a070c77f315ef78f4e
Morgan Yost <yost@calpoly.edu>
2016-02-22 14:33:47 -0800

    renamed utilities (again.. its a hard word to spell)

--- UtilitiesUnitTest/Properties/AssemblyInfo.cs
+++ UtilitiesUnitTest/Properties/AssemblyInfo.cs
@@ -1,41 +1,3 @@
-<<<<<<< HEAD
-﻿using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
-
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("UtilitiesUnitTest")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("California Polytechnic State University")]
-[assembly: AssemblyProduct("UtilitiesUnitTest")]
-[assembly: AssemblyCopyright("Copyright © California Polytechnic State University 2016")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("14ebffdd-3359-4440-bcee-dcc019e4bea8")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
-=======
 ﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
@@ -58,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("fdc7dfad-9fb1-47c7-9400-c0f6bea5f489")]
+[assembly: Guid("89680083-21cd-4fe8-a477-1f6884ad2e0e")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -72,4 +34,3 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
->>>>>>> 717c152f1f0ef286bbf2bbd8bb057e490ecae3dc
--- UtilitiesUnitTest/UnitTest1.cs
+++ UtilitiesUnitTest/UnitTest1.cs
@@ -1,35 +1,21 @@
-<<<<<<< HEAD
-﻿using System;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
-using Utilities;
+﻿using System; 
+using Microsoft.VisualStudio.TestTools.UnitTesting; 
+using Utilities; 
+
 
 namespace UtilitiesUnitTest
-{
-    [TestClass]
-    public class UnitTest1
-    {
-        [TestMethod]
+{ 
+   [TestClass] 
+   public class UnitTest1
+    { 
+        [TestMethod] 
         public void TestMethod1()
-        {
-            string arrayData = "[1, 2.3; 0, -2.1]";
-            Matrix M = new Matrix(arrayData);
+        { 
+            string arrayData = "[1, 2.3; 0, -2.1]"; 
+            Matrix M = new Matrix(arrayData); 
 
-        }
-    }
-}
-=======
-﻿using System;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
 
-namespace UtilitiesUnitTest
-{
-    [TestClass]
-    public class UnitTest1
-    {
-        [TestMethod]
-        public void TestMethod1()
-        {
-        }
-    }
-}
->>>>>>> 717c152f1f0ef286bbf2bbd8bb057e490ecae3dc
+        } 
+    } 
+} 
+  

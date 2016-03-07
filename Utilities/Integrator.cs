aa8d7bcd6fc87ad0e9c015dfd1d6acc2a8835e89
Eric Mehiel <emehiel@calpoly.edu>
2016-03-07 10:21:59 -0800

    Updates to Integrator and Matrix

--- Utilities/Integrator.cs
+++ Utilities/Integrator.cs
@@ -28,8 +28,47 @@ namespace Utilities
 
         private static Matrix<double> RK45Helper(EOMS dynamics, Matrix<double> timeSpanData, Matrix<double> initialState, IntegratorOptions options)
         {
-            Double expon = 1.0 / 5.0;
-            Double temph = 0.0;
+
+            // Make timeSpanData and initialState Column vectors
+            if (timeSpanData.NumRows < timeSpanData.NumCols)
+                timeSpanData = Matrix<double>.Transpose(timeSpanData);
+
+            if (initialState.NumRows < initialState.NumCols)
+                initialState = Matrix<double>.Transpose(initialState);
+
+            int nsteps = 0;
+            int nfailed = 0;
+            int nfevals = 0;
+            int refine = 4;
+            int nout = 0;
+            int nout_old = 0;
+            int nout_new = 0;
+            int neq = initialState.Length;
+
+            Matrix<double> S = new Matrix<double>(new double[,] { { 1.0, 2.0, 3.0 } }) / refine;
+
+            double t0 = timeSpanData[1, 1];
+            double tFinal = timeSpanData[2, 1];
+            double tSpan = tFinal - t0;
+            Matrix<double> tref = new Matrix<double>(1, 1);
+            Matrix<double> tout_new = new Matrix<double>(1, refine);
+
+            double t = t0;
+            Matrix<double> y = initialState;
+            Matrix<double> yout_new = new Matrix<double>(neq, 1);
+
+            MatrixIndex idx = new MatrixIndex();
+
+            int chunk = (int)System.Math.Min(System.Math.Max(100, 50 * refine), refine + System.Math.Floor(System.Math.Pow(2, 13) / neq));
+            Matrix<double> tout = new Matrix<double>(1, chunk);
+            Matrix<double> yout = new Matrix<double>(neq, chunk);
+
+            nout = 1;
+            tout[nout] = t;
+            yout[":", nout] = y;
+
+            double expon = 1.0 / 5.0;
+            double temph = 0.0;
             Matrix<double> A = new Matrix<double>(new double[,] { { 1.0 / 5.0, 3.0 / 10.0, 4.0 / 5.0, 8.0 / 9.0, 1.0, 1.0 } }); // 1x6
             Matrix<double> B = new Matrix<double>(new double[,] { {1.0/5.0, 3.0/40.0,  44.0/45.0,  19372.0/6561.0,  9017.0/3168.0,   35.0/384.0}, //7x6
 					                                  {0.0,     9.0/40.0, -56.0/15.0, -25360.0/2187.0, -355.0/33.0,      0.0},
@@ -41,62 +80,20 @@ namespace Utilities
 
             Matrix<double> hA = new Matrix<double>(A.Size[1], A.Size[2]);
             Matrix<double> hB = new Matrix<double>(B.Size[1], B.Size[2]);
-            Matrix<double> E = new Matrix<double>(new double[,] { { 71.0 / 57600.0, 0.0, -71.0 / 16695.0, 71.0 / 1920.0, -17253.0 / 339200.0, 22.0 / 525.0, -1.0 / 40.0 } }); // 7x1
+            Matrix<double> E = new Matrix<double>(new double[,] { { 71.0 / 57600.0 }, { 0.0 }, { -71.0 / 16695.0 }, { 71.0 / 1920.0 }, { -17253.0 / 339200.0 }, { 22.0 / 525.0 }, { -1.0 / 40.0 } }); // 7x1
 
-            //Complex neq_temp = (Matrix<T>.Max(initialState.Size))[1].Re;
-            int neq = Matrix<int>.Max(initialState.Size)[1];
-            //short neq = (Double)(Matrix<T>.Max(initialState.Size)[1]);
             Matrix<double> f = new Matrix<double>(neq, 7);
 
-            double t0;
-            double tFinal;
-            int ntstep;
+            double htSpan = System.Math.Abs(tSpan);
+            double hmax = System.Math.Min(htSpan, 0.1 * htSpan);
+            double hmin = 16*options.eps;
+            double absh = System.Math.Min(hmax, htSpan);
 
-            Matrix<double> timeSpan;
-
-            // Make timeSpanData and initialState Column vectors
-            if (timeSpanData.NumRows > timeSpanData.NumCols)
-                timeSpanData = Matrix<double>.Transpose(timeSpanData);
-
-            if (initialState.NumRows > initialState.NumCols)
-                initialState = Matrix<double>.Transpose(initialState);
-
-            int nr = timeSpanData.Size[1, 1];
-            int nc = timeSpanData.Size[1, 2];
-
-            if (System.Math.Max(nr, nc) > 2)
-            {
-                t0 = timeSpanData[1, 1];
-                tFinal = timeSpanData[1, nc];
-                ntstep = nc;
-                timeSpan = timeSpanData;
-            }
-            else
-            {
-                t0 = timeSpanData[1, 1];
-                tFinal = timeSpanData[1, 2];
-
-                ntstep = options.nSteps; ;
-                Matrix<double> tvals = new Matrix<double>(1, ntstep + 1);
-                double tstep = (tFinal - t0) / (ntstep);
-
-                for (int i = 1; i <= ntstep + 1; i++)
-                    tvals[1, i] = tstep * (i - 1) + t0;
-
-                timeSpan = tvals;
-            }
-
-            Matrix<double> SolverReturnMatrix = new Matrix<double>(timeSpan.Size[1], initialState.Size[2] + 1);
-
-            Double hmin = options.eps;
-            Double hmax = 0.1 * (tFinal - t0);
-            Double rtol = options.rtol;
-            Double atol = options.atol;
+            double rtol = options.rtol;
+            double atol = options.atol;
             int tdir = 1;
 
-            uint nfailed = 0;
-            uint nfevals = 0;
-            Double threshold = atol / rtol;
+            double threshold = atol / rtol;
             int next = 2;
 
             if ((tFinal - t0) > 0)
@@ -106,10 +103,10 @@ namespace Utilities
 
             // Compute an initial step size h using y'(t).
             Matrix<double> f0 = dynamics[t0, initialState];
-
-            Double absh = System.Math.Abs(hmax);
-
+            
             Matrix<double> temp = Matrix<double>.Max(Matrix<double>.Abs(initialState), threshold);
+
+            // TODO: Assert and throw expection if fo and temp are not the same size (or force f0 and temp to be the same size)
             double tempSingle = (double)Matrix<double>.Max(Matrix<double>.Abs(f0 / temp));
             double rh = tempSingle / (0.8 * System.Math.Pow(rtol, expon));
 
@@ -123,9 +120,7 @@ namespace Utilities
 
             // THE MAIN RK45 LOOP
 
-            Matrix<double> y = initialState;
             Matrix<double> ynew = y;
-            double t = t0;
             double tnew = t;
             bool done = false;
             bool nofailed = true;
@@ -133,15 +128,11 @@ namespace Utilities
             double err = 0.0;
             double h;
 
-            SolverReturnMatrix[nStep, 1] = t0;
-            SolverReturnMatrix[nStep, new MatrixIndex(2, (int)y.Size[2])] = y;
-
             while (!done)
             {
-
                 // By default, hmin is a small number such that t+hmin is only slightly
                 // different than t.  It might be 0 if t is 0.
-                hmin = options.h;
+                hmin = 16*options.eps;
                 //hmin = args.getParam("h");
                 absh = System.Math.Min(hmax, System.Math.Max(hmin, absh));
                 //absh = min(hmax, max(hmin, absh));
@@ -164,30 +155,24 @@ namespace Utilities
                     hA = h * A;
                     hB = h * B;
 
+                    for (int i = 1; i < 6; i++)
+                        f[":", i + 1] = dynamics[t + hA[1, i], y + f * hB[":", i]];
 
-
-                    //f.setColumn(2, inFunction->operator()(t+hA(1,1),y+f*hB(colon(1,7),1)));
-                    //f.setColumn(3, inFunction->operator()(t+hA(1,2),y+f*hB(colon(1,7),2)));
-                    //f.setColumn(4, inFunction->operator()(t+hA(1,3),y+f*hB(colon(1,7),3)));
-                    //f.setColumn(5, inFunction->operator()(t+hA(1,4),y+f*hB(colon(1,7),4)));
-                    //f.setColumn(6, inFunction->operator()(t+hA(1,5),y+f*hB(colon(1,7),5)));
-
-                    tnew = t + (Double)hA[1, 6];
+                    tnew = t + hA[1, 6];
                     if (done)
                         tnew = tFinal;   // Hit end point exactly.
 
                     h = tnew - t;      // Purify h
 
-                    //ynew = y + f*hB[colon(1,7),6];
                     ynew = y + f * hB[":", 6];
-                    //f.setColumn(7, inFunction->operator()(tnew, ynew));
+
+                    f[":", 7] = dynamics[tnew, ynew];
 
                     nfevals = nfevals + 6;
 
-                    //			err = absh * norm(      (f * E) ./ max (max ( abs(y), abs(ynew)), threshold),inf);
-                    err = absh * Matrix<double>.Max(Matrix<double>.Abs(f * E) / Matrix<double>.Max(Matrix<double>.Max(Matrix<double>.Abs(y), Matrix<double>.Abs(ynew))))[1];
-                    //err = (absh * mmax(mabs( (f * E)  / mmax(mmax( mabs(y),mabs(ynew) ), threshold) )))(1);
-
+                    //err = absh * norm(      (f * E) ./ max (max ( abs(y), abs(ynew)), threshold),inf);
+                    err =(double) Matrix<double>.Max(Matrix<double>.Abs((f * E) / Matrix<double>.Max(Matrix<double>.Max(Matrix<double>.Abs(y), Matrix<double>.Abs(ynew)), threshold)));
+                    err = absh * err;
 
                     //    % Accept the solution only if the weighted error is no more than the
                     //    % tolerance rtol.  Estimate an h that will yield an error of rtol on
@@ -215,9 +200,28 @@ namespace Utilities
                     }
                     else  //  Successful step
                     {
-                        nStep++;
                         break;
                     }
+                }
+                nsteps++;
+
+                tref = t + (tnew - t) * S;
+                nout_new = refine;
+                tout_new = Matrix<double>.Horzcat(tref, tnew);
+                yout_new = Matrix<double>.Horzcat(ntrp45(tref, t, y, h, f), ynew);
+
+                if (nout_new > 0)
+                {
+                    nout_old = nout;
+                    nout += nout_new;
+                    if (nout > tout.Length)
+                    {
+                        tout = Matrix<double>.Horzcat(tout, new Matrix<double>(1, chunk));
+                        yout = Matrix<double>.Horzcat(yout, new Matrix<double>(neq, chunk));
+                    }
+                    idx = new MatrixIndex(nout_old + 1, nout);
+                    tout[1, idx] = tout_new;
+                    yout[":", idx] = yout_new;
                 }
 
                 if (nofailed)
@@ -233,20 +237,17 @@ namespace Utilities
                 t = tnew;
                 y = ynew;
 
-                //newMap.ODE_RESULT[t] = y;
-                SolverReturnMatrix[nStep, 1] = t;
-                SolverReturnMatrix[nStep, new MatrixIndex(2, neq + 1)] = y;
-                Console.WriteLine("Integrator State:  {0} done\r", 100 * t / (tFinal - t0));
-
-                //f.setColumn(1,f(colon(1,neq),7));       // Already evaluated feval(odeFcn,tnew,ynew,integratorArgs)
                 f[":", 1] = f[":", 7];
             }
 
-            Console.WriteLine();
+            Matrix<double> SolverReturnMatrix = new Matrix<double>(neq + 1, nout);
+
+            SolverReturnMatrix[1, ":"] = tout[1, new MatrixIndex(1, nout)];
+            SolverReturnMatrix[new MatrixIndex(2, neq + 1), ":"] = yout[":", new MatrixIndex(1, nout)];
             return SolverReturnMatrix;
         }
 
-        private static Matrix<double> ntrp45(double tinterp, double t, Matrix<double> y, double h, Matrix<double> f)
+        private static Matrix<double> ntrp45(Matrix<double> tinterp, double t, Matrix<double> y, double h, Matrix<double> f)
         {
 	        Matrix<double> BI = new Matrix<double>(new double[,] {{1.0,	-183.0/64.0,     37.0/12.0,    -145.0/128.0},
 					                              {0.0,  0.0,            0.0,           0.0},
@@ -257,10 +258,22 @@ namespace Utilities
                                                   {0.0,  3.0/2.0,       -4.0,           5.0/2.0}});
 
 
-            double s = (tinterp - t)/h;  
-	        Matrix<double> S = new Matrix<double>(4,1,s);
+            Matrix<double> s = (tinterp - t)/h;
+            Matrix<double> S = new Matrix<double>(4, s.NumCols);
 
-	        return y + f*(h*BI)*Matrix<double>.Cumprod(S);
+            for (int i = 1; i <= 4; i++)
+                S.SetRow(i, s);
+
+            Matrix<double> Y = new Matrix<double>(y.NumRows, tinterp.NumCols);
+
+            for (int i = 1; i <= Y.NumCols; i++)
+                Y.SetColumn(i, y);
+
+            Matrix<double> yinterp = new Matrix<double>(y.NumRows, tinterp.NumCols);
+
+            yinterp = Y + f*(h*BI)*Matrix<double>.Cumprod(S);
+
+            return yinterp;
         }
     }
 }

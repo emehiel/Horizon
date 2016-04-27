a4a522a368cf1df81565b29f14ef70206f980e89
Morgan Yost <morgan.yost125@gmail.com>
2016-04-27 16:15:35 -0700

    adding model xml file required to run

--- System_H/Logger.cs
+++ System_H/Logger.cs
@@ -2,7 +2,7 @@
 
 namespace HSFSubsystem
 {
-    /// <summary>
+    /// <summary> 
     /// Logger maintains the log data from each failed schedule, writes to csv for post processing and modifies the 
     /// subsystem checking order and constraint checking order to make sure failed subsystems and constraints are evaluated first
     /// </summary>

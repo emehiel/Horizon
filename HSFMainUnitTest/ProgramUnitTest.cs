c4b2059a0187e8cd741eb1c8792107eb2b471c7c
Steven <smmaclea@calpoly.edu>
2017-01-13 13:02:02 -0800

    Init Unit Tests

--- HSFMainUnitTest/ProgramUnitTest.cs
+++ HSFMainUnitTest/ProgramUnitTest.cs
@@ -16,243 +16,119 @@ namespace HSFMainUnitTest
     [TestClass]
     public class ProgramUnitTest
     {
+        /// <summary>
+        /// Test to make sure the correct number of targets are created
+        /// </summary>
         [TestMethod]
-        public void TestScheduleScore()
+        public void TestLoadTargets()
         {
-            var simulationInputFilePath = @"..\..\..\UnitTestInputs\UnitTestSimulationInput.XML";
-            var targetDeckFilePath = @"..\..\..\UnitTestInputs\UnitTestTargets.xml";
-            var modelInputFilePath = @"..\..\..\UnitTestInputs\UnitTestModel.xml";
+            string simulationInputFilePath = @"..\..\..\UnitTestInputs\UnitTestSimulationInput.XML";
+            string targetDeckFilePath = @"..\..\..\UnitTestInputs\UnitTestTargets.xml";
+            string modelInputFilePath = @"..\..\..\UnitTestInputs\UnitTestModel.xml";
 
             string[] inputArg = { "-s", simulationInputFilePath, "-t", targetDeckFilePath, "-m", modelInputFilePath };
 
-            MainTest TestProgram = new MainTest();
+            Program TestProgram = new Program();
 
-            TestProgram.SetInputPaths(inputArg);
-            TestProgram.SetUpOutput();
-            TestProgram.LoadTargets();
-            TestProgram.SetUpDep();
-            TestProgram.SetUpSubsystems();
-            TestProgram.SetUpSchedules();
-            TestProgram.EvaluateSchedule();
-            //Program.Main(inputArg);
-            //Console.WriteLine("Temp");
-            //int index = 
+            TestProgram.InitInput(inputArg);
+            Stack<Task> systemTasks = TestProgram.LoadTargets();
+
+            double actual = systemTasks.Count;
+            double expected = 17;
+
+            Assert.AreEqual(expected, actual);
+
         }
-
-
-    }
-
-    // Helper Functions
-
-
-	public class MainTest
-	{
-        public string outputPath;
-        public string simulationInputFilePath;
-        public string targetDeckFilePath;
-        public string modelInputFilePath;
-        public XmlNode modelInputXMLNode;
-        public Stack<Task> systemTasks;
-        public List<Asset> assetList;
-        public Universe SystemUniverse;
-        XmlNode evaluatorNode;
-        SystemClass simSystem;
-        List<SystemSchedule> schedules;
-        Evaluator schedEvaluator;
-        List<Subsystem> subList;
-        //Create singleton dependency dictionary
-        Dependency dependencies = Dependency.Instance;
-        // Maps used to set up preceeding nodes
-        public Dictionary<ISubsystem, XmlNode> subsystemXMLNodeMap = new Dictionary<ISubsystem, XmlNode>();
-        public Dictionary<string, Subsystem> subsystemMap = new Dictionary<string, Subsystem>();
-        public List<KeyValuePair<string, string>> dependencyMap = new List<KeyValuePair<string, string>>();
-        public List<KeyValuePair<string, string>> dependencyFcnMap = new List<KeyValuePair<string, string>>();
-        // Create Constraint list 
-        List<Constraint> constraintsList = new List<Constraint>();
-        //Create Lists to hold all the initial condition and dependency nodes to be parsed later
-        List<XmlNode> ICNodes = new List<XmlNode>();
-        List<XmlNode> DepNodes = new List<XmlNode>();
-        SystemState initialSysState = new SystemState();
-        public double maxSched;
-
-        public void LoadTargets()
+        /// <summary>
+        /// Test to verify the correct dependencies are loaded
+        /// </summary>
+        [TestMethod]
+        public void TestDependencies()
         {
-            // Find the main input node from the XML input files
-            evaluatorNode = XmlParser.ParseSimulationInput(simulationInputFilePath);
 
-            // Load the target deck into the targets list from the XML target deck input file
-            systemTasks = new Stack<Task>();
-            bool targetsLoaded = Task.loadTargetsIntoTaskList(XmlParser.GetTargetNode(targetDeckFilePath), systemTasks);
-            if (!targetsLoaded)
-            {
-                throw new Exception("Targets were not loaded.");
-            }
-            modelInputXMLNode = XmlParser.GetModelNode(modelInputFilePath);
+            string simulationInputFilePath = @"..\..\..\UnitTestInputs\UnitTestSimulationInput.XML"; // TODO: Update
+            string targetDeckFilePath = @"..\..\..\UnitTestInputs\UnitTestTargets.xml"; // TODO: Update
+            string modelInputFilePath = @"..\..\..\UnitTestInputs\UnitTestModel.xml"; //TODO: Update
+
+            string[] inputArg = { "-s", simulationInputFilePath, "-t", targetDeckFilePath, "-m", modelInputFilePath };
+
+            Program program = new Program();
+            program.InitInput(inputArg);
+            Stack<Task> systemTasks = program.LoadTargets();
+            program.LoadSubsystems();
+            program.LoadDependencies();
+            Assert.Inconclusive("Not Implemented");
         }
-
-
-        public void EvaluateSchedule()
+        /// <summary>
+        /// Test to verify that the correct Constraints are created
+        /// </summary>
+        [TestMethod]
+        public void TestConstraints()
         {
-            foreach (SystemSchedule systemSchedule in schedules)
-            {
-                systemSchedule.ScheduleValue = schedEvaluator.Evaluate(systemSchedule);
-                //bool canExtendUntilEnd = true;
-                // Extend the subsystem states to the end of the simulation 
-                foreach (var subsystem in simSystem.Subsystems)
-                {
-                    if (systemSchedule.AllStates.Events.Count > 0)
-                        if (!subsystem.CanExtend(systemSchedule.AllStates.Events.Peek(), simSystem.Environment, SimParameters.SimEndSeconds)) ;
-                            //log.Error("Cannot Extend " + subsystem.Name + " to end of simulation");
-                }
-            }
+            string simulationInputFilePath = @"..\..\..\UnitTestInputs\UnitTestSimulationInput.XML"; // TODO: Update
+            string targetDeckFilePath = @"..\..\..\UnitTestInputs\UnitTestTargets.xml"; // TODO: Update
+            string modelInputFilePath = @"..\..\..\UnitTestInputs\UnitTestModel.xml"; //TODO: Update
 
-            // Sort the sysScheds by their values
-            schedules.Sort((x, y) => x.ScheduleValue.CompareTo(y.ScheduleValue));
-            schedules.Reverse();
-            maxSched = schedules[0].ScheduleValue;
+            string[] inputArg = { "-s", simulationInputFilePath, "-t", targetDeckFilePath, "-m", modelInputFilePath };
+
+            Program TestProgram = new Program();
+
+            Program program = new Program();
+            program.InitInput(inputArg);
+            Stack<Task> systemTasks = program.LoadTargets();
+            program.LoadSubsystems();
+            Assert.Inconclusive("Not Implemented");
         }
-
-        public void SetUpSubsystems()
+        /// <summary>
+        /// Test to make sure the correct number of schedules are generated
+        /// </summary>
+        [TestMethod]
+        public void TestScheduleCount()
         {
-            assetList = new List<Asset>();
-            subList = new List<Subsystem>();
-            // Set up Subsystem Nodes, first loop through the assets in the XML model input file
-            foreach (XmlNode modelChildNode in modelInputXMLNode.ChildNodes)
-            {
-                if (modelChildNode.Name.Equals("ENVIRONMENT"))
-                {
-                    // Create the Environment based on the XMLNode
-                    SystemUniverse = new Universe(modelChildNode);
-                }
-                if (modelChildNode.Name.Equals("ASSET"))
-                {
-                    Asset asset = new Asset(modelChildNode);
-                    assetList.Add(asset);
-                    // Loop through all the of the ChildNodess for this Asset
-                    foreach (XmlNode childNode in modelChildNode.ChildNodes)
-                    {
-                        // Get the current Subsystem XML Node, and create it using the SubsystemFactory
-                        if (childNode.Name.Equals("SUBSYSTEM"))
-                        {  //is this how we want to do this?
-                           // Check if the type of the Subsystem is scripted, networked, or other
-                            string subName = SubsystemFactory.GetSubsystem(childNode, dependencies, asset, subsystemMap);
-                            foreach (XmlNode ICorDepNode in childNode.ChildNodes)
-                            {
-                                if (ICorDepNode.Name.Equals("IC"))
-                                    ICNodes.Add(ICorDepNode);
-                                if (ICorDepNode.Name.Equals("DEPENDENCY"))
-                                {
-                                    string depSubName = "", depFunc = "";
-                                    depSubName = Subsystem.parseNameFromXmlNode(ICorDepNode, asset.Name);
-                                    dependencyMap.Add(new KeyValuePair<string, string>(subName, depSubName));
+            string simulationInputFilePath = @"..\..\..\UnitTestInputs\UnitTestSimulationInput.XML"; // TODO: Update
+            string targetDeckFilePath = @"..\..\..\UnitTestInputs\UnitTestTargets.xml"; // TODO: Update
+            string modelInputFilePath = @"..\..\..\UnitTestInputs\UnitTestModel.xml"; //TODO: Update
 
-                                    if (ICorDepNode.Attributes["fcnName"] != null)
-                                    {
-                                        depFunc = ICorDepNode.Attributes["fcnName"].Value.ToString();
-                                        dependencyFcnMap.Add(new KeyValuePair<string, string>(subName, depFunc));
-                                    }
-                                }
-                            }
-                        }
-                        //Create a new Constraint
-                        if (childNode.Name.Equals("CONSTRAINT"))
-                        {
-                            constraintsList.Add(ConstraintFactory.GetConstraint(childNode, subsystemMap, asset));
-                        }
-                    }
-                    if (ICNodes.Count > 0)
-                        initialSysState.Add(SystemState.setInitialSystemState(ICNodes, asset));
-                    ICNodes.Clear();
-                }
-            }
-            if (SystemUniverse == null)
-                SystemUniverse = new Universe();
-            foreach (KeyValuePair<string, Subsystem> sub in subsystemMap)
-            {
-                if (!sub.Value.GetType().Equals(typeof(ScriptedSubsystem)))//let the scripted subsystems add their own dependency collector
-                    sub.Value.AddDependencyCollector();
-                subList.Add(sub.Value);
-            }
+            string[] inputArg = { "-s", simulationInputFilePath, "-t", targetDeckFilePath, "-m", modelInputFilePath };
+
+            Program TestProgram = new Program();
+
+            Program program = new Program();
+            program.InitInput(inputArg);
+            Stack<Task> systemTasks = program.LoadTargets();
+            program.LoadSubsystems();
+            program.LoadDependencies();
+            program.CreateSchedules(systemTasks);
+
+            double actual = program.schedules.Count;
+            double expected = 18; 
+            Assert.AreEqual(expected, actual);
         }
-        
-        public void SetUpDep()
+        /// <summary>
+        /// Test to make sure that the maximum schedule score is generated
+        /// </summary>
+        [TestMethod]
+        public void TestScheduleScore()
         {
-            //Add all the dependent subsystems to the dependent subsystem list of the subsystems
-            foreach (KeyValuePair<string, string> depSubPair in dependencyMap)
-            {
-                Subsystem subToAddDep, depSub;
-                subsystemMap.TryGetValue(depSubPair.Key, out subToAddDep);
-                subsystemMap.TryGetValue(depSubPair.Value, out depSub);
-                subToAddDep.DependentSubsystems.Add(depSub);
-            }
+            string simulationInputFilePath = @"..\..\..\UnitTestInputs\UnitTestSimulationInput.XML";
+            string targetDeckFilePath = @"..\..\..\UnitTestInputs\UnitTestTargets.xml";
+            string modelInputFilePath = @"..\..\..\UnitTestInputs\UnitTestModel.xml";
 
-            //give the dependency functions to all the subsytems that need them
-            foreach (KeyValuePair<string, string> depFunc in dependencyFcnMap)
-            {
-                Subsystem subToAddDep;
-                subsystemMap.TryGetValue(depFunc.Key, out subToAddDep);
-                subToAddDep.SubsystemDependencyFunctions.Add(depFunc.Value, dependencies.GetDependencyFunc(depFunc.Value));
-            }
-        }
+            string[] inputArg = { "-s", simulationInputFilePath, "-t", targetDeckFilePath, "-m", modelInputFilePath };
 
-        public void SetUpSchedules()
-        {
-            simSystem = new SystemClass(assetList, subList, constraintsList, SystemUniverse);
+            Program program = new Program();
+            program.InitInput(inputArg);
+            Stack<Task> systemTasks = program.LoadTargets();
+            program.LoadSubsystems();
+            program.LoadDependencies();
+            program.CreateSchedules(systemTasks);
+            double maxSched = program.EvaluateSchedules();
 
-            if (simSystem.CheckForCircularDependencies())
-                throw new NotFiniteNumberException("System has circular dependencies! Please correct then try again.");
+            double actual = maxSched;
+            double expected = 20; //TODO: Find Actual schedule score that should be generated
 
-            schedEvaluator = EvaluatorFactory.GetEvaluator(evaluatorNode, dependencies);
-            Scheduler scheduler = new Scheduler(schedEvaluator);
-            schedules = scheduler.GenerateSchedules(simSystem, systemTasks, initialSysState);
-            // Evaluate the schedules and set their values
-        }
+            Assert.AreEqual(expected, actual);
 
-        public void SetInputPaths(string[] args)
-        { 
-            // Set Defaults
-            simulationInputFilePath = @"..\..\..\SimulationInput.XML";
-            targetDeckFilePath = @"..\..\..\v2.2-300targets.xml";
-            modelInputFilePath = @"..\..\..\DSAC_Static.xml";
-
-            // Get the input filenames
-            int i = 0;
-                foreach(var input in args)
-                {
-                    i++;
-                    switch (input)
-                    {
-                        case "-s":
-                            simulationInputFilePath = args[i];
-                            break;
-                        case "-t":
-                            targetDeckFilePath = args[i];
-                            break;
-                        case "-m":
-                            modelInputFilePath = args[i];
-                            break;
-                    }
-                }
-        }
-        public void SetUpOutput()
-        {
-            // Initialize Output File
-            var outputFileName = string.Format("output-{0:yyyy-MM-dd}-*", DateTime.Now);
-            outputPath = @"C:\HorizonLog\";
-            var txt = ".txt";
-            string[] fileNames = System.IO.Directory.GetFiles(outputPath, outputFileName, System.IO.SearchOption.TopDirectoryOnly);
-            double number = 0;
-            foreach (var fileName in fileNames)
-            {
-                char version = fileName[fileName.Length - txt.Length - 1];
-                if(number<Char.GetNumericValue(version))
-                    number = Char.GetNumericValue(version);
-            }
-            number++;
-            outputFileName = outputFileName.Remove(outputFileName.Length - 1) + number;
-            outputPath += outputFileName + txt;
         }
     }
-
-
 }

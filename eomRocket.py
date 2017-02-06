a2099f9d3f2ed499dc78db7309be27b8486e4637
steve <steve@STEVEN-LAPTOP>
2017-02-05 17:00:48 -0800

    Begin adding drag

--- eomRocket.py
+++ eomRocket.py
@@ -12,20 +12,23 @@ from Utilities import *
 from System.Collections.Generic import Dictionary
 from IronPython.Compiler import CallTarget0
 
-class eom(Utilities.EOMS):
+class eomRocket(Utilities.EOMS):
     def __init__(self):
         pass
     def PythonAccessor(self, t, y):
         _mu = 398600
         r3 = System.Math.Pow(Matrix[System.Double].Norm(y[MatrixIndex(1, 3), 1]), 3)
         mur3 = -_mu / r3
-        _A = Vector[System.Double]()
-        _A[1, 4] = 1.0
-        _A[2, 5] = 1.0
-        _A[3, 6] = 1.0
-        _A[4, 1] = mur3
-        _A[5, 2] = mur3
-        _A[6, 3] = mur3
+        temp = self.DragCalculation(t, y)
+        _A = Matrix[System.Double](6)
+        _A[1] = 1.0
+        _A[2] = 1.0
+        _A[3] = 1.0
+        _A[4] = mur3
+        _A[5] = mur3
+        _A[6] = mur3
         dy = Matrix[System.Double]()
         dy = _A * y
         return dy
+    def DragCalculation(self, t, y):
+        return 1

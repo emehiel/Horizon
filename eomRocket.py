bf35f04274ab260fd7a0b85e94be3dfdd7f09ec8
steve <steve@STEVEN-LAPTOP>
2017-02-06 12:37:38 -0800

    more changes to eom python file

--- eomRocket.py
+++ eomRocket.py
@@ -6,9 +6,12 @@ clr.AddReference('System.Core')
 clr.AddReference('IronPython')
 clr.AddReference('System.Xml')
 clr.AddReferenceByName('Utilities')
+clr.AddReferenceByName('HSFUniverse')
 
 import Utilities
+import HSFUniverse
 from Utilities import *
+from HSFUniverse import Atmosphere
 from System.Collections.Generic import Dictionary
 from IronPython.Compiler import CallTarget0
 
@@ -19,7 +22,6 @@ class eomRocket(Utilities.EOMS):
         _mu = 398600
         r3 = System.Math.Pow(Matrix[System.Double].Norm(y[MatrixIndex(1, 3), 1]), 3)
         mur3 = -_mu / r3
-        temp = self.DragCalculation(t, y)
         _A = Matrix[System.Double](6)
         _A[1] = 1.0
         _A[2] = 1.0
@@ -32,3 +34,7 @@ class eomRocket(Utilities.EOMS):
         return dy
     def DragCalculation(self, t, y):
         return 1
+
+class dragRocket
+    cd = 0.5
+    def GetAtmosphere(height)

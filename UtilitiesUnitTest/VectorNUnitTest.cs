14ac12fafc6abe225e09b820110f76eb37348f0c
steve <steve@STEVEN-LAPTOP>
2017-02-07 16:21:13 -0800

    Added NUnit

---
+++ UtilitiesUnitTest/VectorNUnitTest.cs
@@ -0,0 +1,22 @@
+﻿
+using System;
+using NUnit.Framework;
+using Utilities;
+namespace NUnit.Samples.Money
+
+{
+    [TestFixture]
+    public class VectorTest
+    {
+        [Test]
+        public void DotTest()
+        {
+            Vector a = new Vector(new double[] { 7378, 0, 0 });
+            Vector b = new Vector(new double[] { -12525.2938790736, 1346.43186132083, -3517.5443929026 });
+            double c = Vector.Dot(a, b);
+            double d = -92411618;
+
+            Assert.AreEqual(d, c, 1);
+        }
+    }
+}

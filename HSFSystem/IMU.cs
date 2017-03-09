a24bea3c2b7e964555e887e966f03bcf1e6f8394
steve <steve@STEVEN-LAPTOP>
2017-03-08 22:01:59 -0800

    initial IMU setup

--- HSFSystem/IMU.cs
+++ HSFSystem/IMU.cs
@@ -13,7 +13,7 @@ namespace HSFSystem
     class IMU:Subsystem
     {
         #region Attributes
-        protected double _accNoisePower = 0;
+        protected double _accNoiseDensity = 0;
         protected double _accNaturalFrequency = 150;
         protected double _accDampingratio = 0.707;
         protected double _accMax = Double.MaxValue;
@@ -23,9 +23,10 @@ namespace HSFSystem
         protected double _gyrDampingRatio = 0.707;
         protected double _gyrMax = Double.MaxValue;
         protected double _gyrMin = Double.MinValue;
-        protected double _gyrRateWalk = 0;
+        protected double _gyrRateNoiseDensity = 0;
         protected double _gyrBias = 0;
         protected double _gyrOutputRate = 100;
+        static private Random rand = new Random(); 
         #endregion
         public IMU(XmlNode IMUNode, Dependency dependencies, Asset asset)
         {
@@ -48,14 +49,14 @@ namespace HSFSystem
                 _gyrMax = (double)Convert.ChangeType(IMUNode.ChildNodes[gyr].Attributes["gyroMax"].Value.ToString(), typeof(double));
             if (IMUNode.ChildNodes[gyr].Attributes["gyroMin"] != null)
                 _gyrMin = (double)Convert.ChangeType(IMUNode.ChildNodes[gyr].Attributes["gyroMin"].Value.ToString(), typeof(double));
-            if (IMUNode.ChildNodes[gyr].Attributes["gyroRateWalk"] != null)
-                _gyrRateWalk = (double)Convert.ChangeType(IMUNode.ChildNodes[gyr].Attributes["gyroRateWalk"].Value.ToString(), typeof(double));
+            if (IMUNode.ChildNodes[gyr].Attributes["gyroRateNoiseDensity"] != null)
+                _gyrRateNoiseDensity = (double)Convert.ChangeType(IMUNode.ChildNodes[gyr].Attributes["gyroRateNoiseDensity"].Value.ToString(), typeof(double));
             if (IMUNode.ChildNodes[gyr].Attributes["gyroBias"] != null)
                 _gyrBias = (double)Convert.ChangeType(IMUNode.ChildNodes[gyr].Attributes["gyroBias"].Value.ToString(), typeof(double));
             if (IMUNode.ChildNodes[gyr].Attributes["gyroOutputRate"] != null)
                 _gyrOutputRate = (double)Convert.ChangeType(IMUNode.ChildNodes[gyr].Attributes["gyroOutputRate"].Value.ToString(), typeof(double));
-            if (IMUNode.ChildNodes[acc].Attributes["accNoisePower"] != null)
-                _accNoisePower = (double)Convert.ChangeType(IMUNode.ChildNodes[acc].Attributes["accNoisePower"].Value.ToString(), typeof(double));
+            if (IMUNode.ChildNodes[acc].Attributes["accNoiseDensity"] != null)
+                _accNoiseDensity = (double)Convert.ChangeType(IMUNode.ChildNodes[acc].Attributes["accNoiseDensity"].Value.ToString(), typeof(double));
             if (IMUNode.ChildNodes[acc].Attributes["accNaturalFrequency"] != null)
                 _accNaturalFrequency = (double)Convert.ChangeType(IMUNode.ChildNodes[acc].Attributes["accNaturalFrequency"].Value.ToString(), typeof(double));
             if (IMUNode.ChildNodes[acc].Attributes["accDampingRatio"] != null)
@@ -70,11 +71,25 @@ namespace HSFSystem
         }
         public double Gyroscope()
         {
+            double noise = GaussianWhiteNoise(0, _gyrRateNoiseDensity*_gyrOutputRate);
             throw new NotImplementedException();
         }
         public double Accelerometer()
         {
+            double noise = GaussianWhiteNoise(0, _accNoiseDensity*_accOutputRate);
             throw new NotImplementedException();
+        }
+
+        public double GaussianWhiteNoise(double mean, double stdDev)
+        // http://stackoverflow.com/a/218600
+        {
+            double u1 = 1.0 - rand.NextDouble(); //uniform(0,1] random doubles
+            double u2 = 1.0 - rand.NextDouble();
+            double randStdNormal = Math.Sqrt(-2.0 * Math.Log(u1)) *
+                         Math.Sin(2.0 * Math.PI * u2); //random normal(0,1)
+            double randNormal =
+                         mean + stdDev * randStdNormal; //random normal(mean,stdDev^2)
+            return randNormal;
         }
     }
 }

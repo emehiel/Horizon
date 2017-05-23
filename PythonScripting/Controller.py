fde42428ce773d3a204ab9997c1ac09eddb230c6
steve <steve@STEVEN-LAPTOP>
2017-05-22 17:23:22 -0700

    Get gain from XML, outputs a deflection angle, not delta

--- PythonScripting/Controller.py
+++ PythonScripting/Controller.py
@@ -33,19 +33,13 @@ from IronPython.Compiler import CallTarget0
 class Controller(Subsystem):
     def __init__(self, node, asset):
         self.Asset = asset
-        self.aero = AeroPrediction.AeroPrediction()
-
-        self.DELTA_CX= StateVarKey[System.Double](asset.Name + "." + "dcx");
-        self.DELTA_CY= StateVarKey[System.Double](asset.Name + "." + "dcy");
-        self.DELTA_CZ = StateVarKey[System.Double](asset.Name + "." + "dcz");
+        matrixstr = node.Attributes["GainMatrix"].Value
+        kest = Matrix[System.Double](matrixstr)
+        self.kest = kest
         self.DEFLECTION_KEY = StateVarKey[Matrix[System.Double]](asset.Name + "." + "deflection")
+        self.DROGUE_DEPLOYED = StateVarKey[System.Boolean](asset.Name + "." + "drogue")
 
-        self.addKey(self.DELTA_CY)
-        self.addKey(self.DELTA_CZ)
         self.addKey(self.DEFLECTION_KEY)
-        self.Asset.AssetDynamicState.IntegratorParameters.Add(self.DELTA_CY, 0)
-        self.Asset.AssetDynamicState.IntegratorParameters.Add(self.DELTA_CZ, 0)
-        self.Asset.AssetDynamicState.IntegratorParameters.Add(self.DELTA_CX, 0)
         self.Asset.AssetDynamicState.IntegratorParameters.Add(self.DEFLECTION_KEY, Matrix[System.Double](4,1))
         self.A = 0.510458340443047
         self.B = -0.489541659556953
@@ -58,116 +52,34 @@ class Controller(Subsystem):
     def GetDependencyCollector(self):
         return Func[Event,  Utilities.HSFProfile[System.Double]](self.DependencyCollector)
     def CanPerform(self, event, universe):
-        #return super(subsystem, self).CanPerform(event, universe)
-        spdOfSnd = 343 #Fixme: Calculate spdofsnd
-        cl = 0
-        cld = 0
-        state = self.DependencyCollector(event).LastValue()
-        state = Matrix[System.Double].Transpose(state)
-        controlState = state[MatrixIndex(7,12), 1]
-
-        kest = Matrix[System.Double](4,6)
-        
-        kest[1,1] = -1.0351
-        kest[2,1] = 1.1795
-        kest[3,1] = -1.0351
-        kest[4,1] = 1.1795
-        
-        kest[1,2] = 1.1761
-        kest[2,2] = -1.0371
-        kest[3,2] = 1.1761
-        kest[4,2] = -1.0371
-
-        kest[1,3] = 1.5098
-        kest[2,3] = 1.5149
-        kest[3,3] = 1.5098
-        kest[4,3] = 1.5149
-        
-        
-        kest[1,4] = 1.5310
-        kest[2,4] = -4.3951
-        kest[3,4] = 1.5310
-        kest[4,4] = -4.3951
-        
-        kest[1,5] = -3.2225
-        kest[2,5] = -54.9069
-        kest[3,5] = -3.2225
-        kest[4,5] = -54.9069
-        
-        kest[1,6] = -8.3853
-        kest[2,6] = -59.4790
-        kest[3,6] = -8.3853
-        kest[4,6] = -59.4790
-        
-        '''
-        kest[1,1] = 1.0768
-        kest[2,1] = 1.1276
-        kest[3,1] = 1.0768
-        kest[4,1] = 1.1284
-        
-        kest[1,2] = 1.1201
-        kest[2,2] = 1.0827
-        kest[3,2] = 1.1201
-        kest[4,2] = 1.0827
-
-        kest[1,3] = 4.6472
-        kest[2,3] = -4.7762
-        kest[3,3] = 4.6472
-        kest[4,3] = -4.7762
-
-        kest[1,4] = -13.6380
-        kest[2,4] = 5.3654
-        kest[3,4] = -13.6380
-        kest[4,4] = 5.3654
-        
-        kest[1,5] = 166.3307
-        kest[2,5] = -68.8198
-        kest[3,5] = 166.3307
-        kest[4,5] = -68.8198
-        
-        kest[1,6] = -159.5715
-        kest[2,6] = 74.0770
-        kest[3,6] = -159.5715
-        kest[4,6] = 74.0770
-        '''
-        deflection = kest*controlState
-       #try:
-            #omega = self.Asset.AssetDynamicState.IntegratorParameters.GetValue(StateVarKey[Vector]("asset1.gyro"))
-        #except:
-            #omega = Vector(3)
-            #print "No gyro key"
-        cn = Vector(4)
-        mach = state[4,1]/spdOfSnd
-        for fin in range(4):
-            if deflection[fin+1,1] - self.deflectionOld[fin+1,1] > self.deltaDeflection:
-                deflection[fin+1,1] = self.deflectionOld[fin+1,1] +  self.deltaDeflection
-            elif deflection[fin+1,1] - self.deflectionOld[fin+1,1] < -self.deltaDeflection:
-                deflection[fin+1,1] = self.deflectionOld[fin+1,1] - self.deltaDeflection
-            deflection[fin+1,1] =  deflection[fin+1,1]*180/Math.PI
-            if deflection[fin+1,1] > 15:
-                deflection[fin+1,1] = 15
-            if deflection[fin+1,1] < -15:
-                deflection[fin+1,1] = -15
-            deflection[fin+1,1] =  deflection[fin+1,1] * Math.PI/180
-            cn[fin+1] = self.aero.NormalForceFin(mach, 0, deflection[fin+1,1]) * Math.Sign(deflection[fin+1,1]) # Fixme: Assume alpha = 0 for now
-            cl += self.aero.RollingForceFin(cn[fin+1], deflection[fin+1,1], mach/spdOfSnd) 
-            #cld += self.aero.RollDampingFin(cn[fin+1], omega[1], mach/spdOfSnd);
-        #dCx = cl + cld
-        #print omega[1], cl, cld, dCx
-        dCx = 0
-        dCy = cn[1] + cn[3]
-        dCz = cn[2] + cn[4]
-
-        self.Asset.AssetDynamicState.IntegratorParameters.Add(self.DELTA_CX, dCx)
-        self.Asset.AssetDynamicState.IntegratorParameters.Add(self.DELTA_CY, dCy)
-        self.Asset.AssetDynamicState.IntegratorParameters.Add(self.DELTA_CZ, dCz)
-        #self.Asset.AssetDynamicState.IntegratorParameters.Add(self.CNTRL_MNT, moment)
         ts = event.GetEventStart(self.Asset)
-        self._newState.AddValue(self.DEFLECTION_KEY, HSFProfile[Matrix[System.Double]](ts, deflection.Transpose(deflection)))
-        self._newState.AddValue(self.DELTA_CY, HSFProfile[System.Double](ts, dCy))
-        self._newState.AddValue(self.DELTA_CZ, HSFProfile[System.Double](ts, dCz))
+        state = self.DependencyCollector(event).LastValue()
+        if not (self.Asset.AssetDynamicState.IntegratorParameters.GetValue(self.DROGUE_DEPLOYED)):
+        #if(True):
+            state = Matrix[System.Double].Transpose(state)
+            controlState = state[MatrixIndex(7,12), 1]
+
+            deflection = -self.kest*controlState
+            for fin in range(4):
+                if deflection[fin+1,1] - self.deflectionOld[fin+1,1] > self.deltaDeflection:
+                    deflection[fin+1,1] = self.deflectionOld[fin+1,1] +  self.deltaDeflection
+                elif deflection[fin+1,1] - self.deflectionOld[fin+1,1] < -self.deltaDeflection:
+                    deflection[fin+1,1] = self.deflectionOld[fin+1,1] - self.deltaDeflection
+                deflection[fin+1,1] =  deflection[fin+1,1]*180/Math.PI
+                if deflection[fin+1,1] > 15:
+                    deflection[fin+1,1] = 15
+                if deflection[fin+1,1] < -15:
+                    deflection[fin+1,1] = -15
+                deflection[fin+1,1] =  deflection[fin+1,1] * Math.PI/180
+
+            self.Asset.AssetDynamicState.IntegratorParameters.Add(self.DEFLECTION_KEY, deflection)
+            
+            self._newState.AddValue(self.DEFLECTION_KEY, HSFProfile[Matrix[System.Double]](ts, deflection.Transpose(deflection)))
         
-        self.deflectionOld = deflection
+            self.deflectionOld = deflection
+            return True
+        deflection = Matrix[System.Double](4,1)
+        self._newState.AddValue(self.DEFLECTION_KEY, HSFProfile[Matrix[System.Double]](ts, deflection.Transpose(deflection)))
         return True
     def CanExtend(self, event, universe, extendTo):
         return True

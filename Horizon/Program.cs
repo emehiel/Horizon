7421cd692376f4334e13eb42e721fb23cf5bbfc3
Eric Mehiel <emehiel@calpoly.edu>
2022-12-12 14:20:03 -0800

    Revert "update default inputs"
    This reverts commit a2c7e5c5213092a42afae3c8e9bc1ee6966e1a7c.

--- Horizon/Program.cs
+++ Horizon/Program.cs
@@ -109,6 +109,7 @@ namespace Horizon
             SimulationInputFilePath = @"..\..\..\SimulationInput.xml";
             TargetDeckFilePath = @"..\..\..\v2.2-300targets.xml";
             ModelInputFilePath = @"..\..\..\DSAC_Static.xml";
+            //ModelInputFilePath = @"..\..\..\Model_Static.xml";
             bool simulationSet = false, targetSet = false, modelSet = false;
 
             // Get the input filenames
@@ -262,6 +263,7 @@ namespace Horizon
                         break;
                 }
             }
+            SystemUniverse = new SpaceEnvironment();
             log.Info("Subsystems and Constraints Loaded");
         }
         public void LoadDependencies()

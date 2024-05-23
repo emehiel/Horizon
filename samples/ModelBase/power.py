d5bf06b883d4335418e0ad1c15fc0637b69bc587
shaniya1 <shaniya.singh14@gmail.com>
2024-05-23 11:15:58 -0700

    Added additional input files; made some changes to power subsystem based on target deck inputs

--- samples/ModelBase/power.py
+++ samples/ModelBase/power.py
@@ -2,6 +2,7 @@ import sys
 import clr
 import System.Collections.Generic
 import System
+import math
 clr.AddReference('System.Core')
 clr.AddReference('IronPython')
 clr.AddReference('System.Xml')
@@ -28,19 +29,6 @@ from System.Collections.Generic import Dictionary
 from IronPython.Compiler import CallTarget0
 
 class power(HSFSystem.Subsystem):
- 
-    def __init__(self, node, asset):
-        super(power, self).__init__(node, asset)
-        self.DOD_KEY = "dod"  # Example, should be initialized appropriately
-        self.POWIN_KEY = "powerin"  # Example, should be initialized appropriately
-        self.batterySize = float(node.Attributes["batterySize"].Value) if node.Attributes["batterySize"] else 1000000
-        self.solarCellEfficiency = float(node.Attributes["solarCellEfficiency"].Value) if node.Attributes["solarCellEfficiency"] else 0.2
-        self.solarCellArea = float(node.Attributes["solarCellArea"].Value) if node.Attributes["solarCellArea"] else 1.0
-        self.fullSolarPower = float(node.Attributes["fullSolarPower"].Value) if node.Attributes["fullSolarPower"] else 150
-        self.penumbraSolarPower = float(node.Attributes["penumbraSolarPower"].Value) if node.Attributes["penumbraSolarPower"] else 75
-        self.initial_time = 0  # Initial time, should be set appropriately
-        self.calculation_interval = 60  # Calculation interval in seconds
-
     def CanPerform(self, event, universe):
         es = event.GetEventStart(self.Asset)
         te = event.GetTaskEnd(self.Asset)
@@ -53,14 +41,20 @@ class power(HSFSystem.Subsystem):
 
         olddod = self._newState.GetLastValue(self.DOD_KEY).Item2
         
+        # Determine if in sunlit or eclipse period based on target (time window)
+        sunlit = self.is_sunlit_period(es, ee, universe)
+        
         # collect power profile out
         powerOut = self.DependencyCollector(event)
         powerOut = powerOut + powerSubPowerOut
 
+        # collect power profile in
         position = self.Asset.AssetDynamicState
-        powerIn = self.calc_solar_panel_power_profiled(es, te, self._newState, position, universe)
+        powerIn = self.CalcSolarPanelPowerProfile(es, te, self._newState, position, universe, sunlit)
+        
+        # calculate dod rate
         dodrateofchange = HSFProfile[System.Double]()
-        dodrateofchange = (powerOut - powerIn) / self.batterySize
+        dodrateofchange = ((powerOut - powerIn) / self.batterySize)
 
         exceeded = False
         freq = 1.0
@@ -79,51 +73,47 @@ class power(HSFSystem.Subsystem):
         else:
             return self.fullSolarPower
 
-    def calc_solar_panel_power_profile(self, start, end, state, position, universe):
+    def CalcSolarPanelPowerProfile(self, start, end, state, position, universe, sunlit):
         freq = 5
-        lastShadow = universe.Sun.castShadowOnPos(position, start)
-        solarPanelSolarProfile = Utilities.HSFProfile[System.Double](start, self.GetSolarPanelPower(lastShadow))
+        solarPanelSolarProfile = Utilities.HSFProfile[System.Double](start, self.fullSolarPower if sunlit else 0)
 
         time = start
         while time <= end:
-            shadow = universe.Sun.castShadowOnPos(position, time)
-            if shadow != lastShadow:
-                solarPanelSolarProfile[time] = self.GetSolarPanelPower(shadow)
-                lastShadow = shadow
+            solarPanelSolarProfile[time] = self.fullSolarPower if sunlit else 0
             time += freq
         state.AddValues(self.POWIN_KEY, solarPanelSolarProfile)
         return solarPanelSolarProfile
 
-    def calc_solar_panel_power_profiled(self, start, end, state, position, universe):
-        solar_output = Utilities.HSFProfile[System.Double]()
-        time = start
-        while time <= end:
-            solar_intensity = self.get_solar_intensity(time, position, universe)
-            cell_efficiency = self.get_cell_efficiency(time)
-            degradation = self.get_degradation_factor(time)
-            power = solar_intensity * cell_efficiency * degradation * self.solarCellArea
-            solar_output[time] = power
-            time += self.calculation_interval
-        state.AddValues(self.POWIN_KEY, solar_output)
-        return solar_output
+    def is_sunlit_period(self, start, end, universe):
+        # Get position of the satellite at the start and end times
+        position_start = self.Asset.AssetDynamicState.PositionAtTime(start) #Location in framework? No asset declared within DynamicState.cs
+        position_end = self.Asset.AssetDynamicState.PositionAtTime(end)
+        
+        # Get the position of the Sun 
+        sun_position = universe.Sun.Position #need to find this
 
-    def get_solar_intensity(self, time, position, universe):
-        return 1361
+        # Check if the satellite is in the Earth's shadow at the start and end times
+        sunlit_start = self.is_in_sunlight(position_start, sun_position, universe)
+        sunlit_end = self.is_in_sunlight(position_end, sun_position, universe)
 
-    def get_cell_efficiency(self, time):
-        initial_efficiency = self.solarCellEfficiency
-        years = (time - self.initial_time) / (365 * 24 * 3600)
-        return initial_efficiency * (1 - 0.005 * years)
+        return sunlit_start or sunlit_end
 
-    def get_degradation_factor(self, time):
-        years = (time - self.initial_time) / (365 * 24 * 3600)
-        return max(0.8, 1 - 0.02 * years)
+    def is_in_sunlight(self, satellite_position, sun_position, universe):
+        # shadow checks, considering the Earth's umbra and penumbra
+        # For simplicity, using a basic geometric check here
+        earth_position = universe.Earth.Position
+        satellite_to_earth = [satellite_position[i] - earth_position[i] for i in range(3)]
+        sun_to_earth = [sun_position[i] - earth_position[i] for i in range(3)]
 
-    def size_energy_storage(self, orbital_params, peak_power_demand, eclipse_period):
-        pass
+        angle = self.angle_between_vectors(satellite_to_earth, sun_to_earth)
+        return angle > 90  # Satellite is in sunlight if the angle is greater than 90 degrees
 
-    def regulate_power(self, power_requirements):
-        pass
+    def angle_between_vectors(self, v1, v2):
+        # Calculate the angle between two vectors
+        dot_product = sum(a * b for a, b in zip(v1, v2))
+        magnitude_v1 = sum(a ** 2 for a in v1) ** 0.5
+        magnitude_v2 = sum(b ** 2 for b in v2) ** 0.5
+        return math.degrees(math.acos(dot_product / (magnitude_v1 * magnitude_v2)))
 
     def DepFinder(self, depFnName):
         fnc = getattr(self, depFnName)
@@ -137,3 +127,4 @@ class power(HSFSystem.Subsystem):
 
     def DependencyCollector(self, currentEvent):
         return super(power, self).DependencyCollector(currentEvent)
+
